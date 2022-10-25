5610d468f28191c919656293a804ac13c34c0fe8
RamadanKher <ramadankher1@gmail.com>
2022-10-25 16:58:41 +0200

    nab

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,3 +3,23 @@ $(window).ready(() => {
     $("#loading").fadeOut(1400)
     $("body").css("overflow", "auto")
 })
+//  close btn for nav
+let toggleMenu = document.getElementById("toggleMenu");
+let LinksList = document.getElementById("LinksList");
+
+
+
+
+
+
+// this for togle btoom  sidbar links in responsive
+toggleMenu.addEventListener("click",() => {
+
+   toggleMenu.classList.toggle("open")
+   LinksList.classList.toggle("open")
+ 
+})
+
+
+
+
